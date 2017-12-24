ab6cbcf1da7e912df29e8416a10ec405eb1eb66d
KevinH <kevin.hosey91@gmail.com>
2017-12-24 20:56:13 +0000

    day 26

---
+++ 026/01_cat_multi_line.sh
@@ -0,0 +1,24 @@
+
+#!/bin/bash
+# Using cat for message blocks
+
+cat <<End-of-message
+-------------------------------------
+This is line 1 of the message.
+This is line 2 of the message.
+This is line 3 of the message.
+This is line 4 of the message.
+This is the last line of the message.
+-------------------------------------
+End-of-message
+
+echo
+
+cat <<End-of-message
+-------------------------------------
+We wish you a Merry Christmas 
+We wish you a Merry Christmas 
+We wish you a Merry Christmas 
+And a Happy New Year!
+-------------------------------------
+End-of-message
