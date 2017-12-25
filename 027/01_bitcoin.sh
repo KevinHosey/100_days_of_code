27eb331b72d571f8ffcdaa472dd776a1f13ff837
KevinH <kevin.hosey91@gmail.com>
2017-12-25 20:21:33 +0000

    day 27

---
+++ 027/01_bitcoin.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Shows btc price from Coindesk.com
+
+clear
+echo "Coindesk BTC: "
+echo "  USD             EUR             GBP "
+
+while [ 1 ] 
+do
+curl -s http://api.coindesk.com/v1/bpi/currentprice.json |  python -c "import json, sys; bitcoin=json.load(sys.stdin); print bitcoin['bpi']['USD']['rate'] + '\t' + bitcoin['bpi']['EUR']['rate'] + '\t' + bitcoin['bpi']['GBP']['rate']"
+printf "\033[A" 
+sleep 5
+done
+
