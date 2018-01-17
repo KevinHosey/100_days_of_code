a2f32458f38dd18863035194fba7901b5a8f8309
KevinHosey <kevin@mimi.io>
2018-01-17 21:21:57 +0100

    day 50

---
+++ 050/01_unset.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Unsetting a variable.
+
+variable=hello_bitcoin                       
+echo "variable = $variable"
+
+unset variable                      
+                                                           
+echo "(unset) variable = $variable"  
+
+if [ -z "$variable" ]                
+then
+  echo "\$variable has length ${#variable}"
+fi
+
+exit 0
